5af34a55df42862f77ba5dd29d47d839a302edfb
Juliano <juliano@mail.com>
2024-12-06 13:30:10 -0300

    Adicionando componentes botão

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import Button from "./Components/Button"
 
 function App() {
   return (
-    <>
-    <Button/>
-    </>
+    <div className="bg-darkBackground">
+      <Button>
+        Primary BackGround
+      </Button> 
+      <Button variant="secondary">
+        Secondary BackGround
+      </Button> 
+      <Button isBold>
+        BoldText with Primary BackGorund
+      </Button>
+      <Button isBold variant='secondary'>
+        BoldText with Secondary Background
+      </Button>
+    </div>
   )
 }
 
--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -1,7 +1,13 @@
-function Button({children}){
+function Button({children, variant ="primary", isBold = false}){
     return(
-        <button>{children}</button>
+        <button className={`flex items-center justify-center p-4 rounded-[35px] font-graphic text-x1 leading-5 ${variant === 'primary' ? "bg-primary text-white" : "bg-white text-primary"} ${isBold && 'font-graphic-bold'}`
+        }>{children}</button>
     )
 }
 
 export default Button
+
+/*
+[] -> Você pode colocar um valor personalizado
+
+*/
