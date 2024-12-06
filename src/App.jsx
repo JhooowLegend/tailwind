3c2bb90ded47ad4f3bf416285f20f74ea4d00137
Jhow636 <86700623+Jhow636@users.noreply.github.com>
2024-12-06 09:56:59 -0300

    Adicionando fonte

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-blue-600'>Teste</h1>
+      <h1 className='text-blue-600 font-graphic'>Teste</h1>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        graphic: ['Graphic', 'sans-serif'],
+        'graphic-bold': ['Graphic-bold', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
